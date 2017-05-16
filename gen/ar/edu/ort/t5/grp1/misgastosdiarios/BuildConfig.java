bc5d82d51106ea41bd73068c13dc85addc556df4
MARIANO GABRIEL PIEDROZZI <29320000@ort>
2017-05-16 20:18:20 -0300

    Inicial

---
+++ gen/ar/edu/ort/t5/grp1/misgastosdiarios/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ar.edu.ort.t5.grp1.misgastosdiarios;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
