8f0a8a0f3d558a420d1cc30dab7b67befb2b4ba0
29320000 <29320000@A-CRO-04.ALMAGRO.ort.arg>
2017-05-23 21:53:09 -0300

    Agregue el layout de Gastos pero hay que completarlo

--- gen/ar/edu/ort/t5/grp1/misgastosdiarios/R.java
+++ gen/ar/edu/ort/t5/grp1/misgastosdiarios/R.java
@@ -26,43 +26,54 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int GridLayout1=0x7f080002;
+        public static final int GridLayout1=0x7f080001;
         public static final int LinearLayout1=0x7f080000;
-        public static final int LinearLayout2=0x7f08000e;
-        public static final int action_settings=0x7f080016;
-        public static final int btnMainActivityNuevoGasto=0x7f080003;
-        public static final int btnMainActivityReportesGastos=0x7f080004;
-        public static final int etReporteActivityAnio=0x7f080006;
-        public static final int etReporteActivityMes=0x7f080005;
-        public static final int lvGastos=0x7f080001;
-        public static final int tableRow1=0x7f080007;
-        public static final int tableRow2=0x7f08000b;
-        public static final int tableRow3=0x7f08000c;
-        public static final int tableRow4=0x7f08000d;
-        public static final int tvListViewGastoCategoria=0x7f080010;
-        public static final int tvListViewGastoDetalle=0x7f080011;
-        public static final int tvListViewGastoFecha=0x7f080012;
-        public static final int tvListViewGastoImporte=0x7f08000f;
-        public static final int tvListViewReporteCategoria=0x7f080013;
-        public static final int tvListViewReporteImporte=0x7f080014;
-        public static final int tvListViewReportePorcentaje=0x7f080015;
-        public static final int tvReporteActivityCategoria=0x7f080008;
-        public static final int tvReporteActivityImporte=0x7f080009;
-        public static final int tvReporteActivityPorcentaje=0x7f08000a;
+        public static final int LinearLayout2=0x7f080014;
+        public static final int action_settings=0x7f08001c;
+        public static final int btnGastosActivityABMCategorias=0x7f080005;
+        public static final int btnGastosActivityAceptar=0x7f080006;
+        public static final int btnGastosActivityCancelar=0x7f080007;
+        public static final int btnMainActivityNuevoGasto=0x7f080009;
+        public static final int btnMainActivityReportesGastos=0x7f08000a;
+        public static final int etGastosActivityDescripcion=0x7f080004;
+        public static final int etGastosActivityImporte=0x7f080003;
+        public static final int etReporteActivityAnio=0x7f08000c;
+        public static final int etReporteActivityMes=0x7f08000b;
+        public static final int lvGastos=0x7f080008;
+        public static final int spnGastosActivityCategoria=0x7f080002;
+        public static final int tableRow1=0x7f08000d;
+        public static final int tableRow2=0x7f080011;
+        public static final int tableRow3=0x7f080012;
+        public static final int tableRow4=0x7f080013;
+        public static final int tvListViewGastoCategoria=0x7f080016;
+        public static final int tvListViewGastoDetalle=0x7f080017;
+        public static final int tvListViewGastoFecha=0x7f080018;
+        public static final int tvListViewGastoImporte=0x7f080015;
+        public static final int tvListViewReporteCategoria=0x7f080019;
+        public static final int tvListViewReporteImporte=0x7f08001a;
+        public static final int tvListViewReportePorcentaje=0x7f08001b;
+        public static final int tvReporteActivityCategoria=0x7f08000e;
+        public static final int tvReporteActivityImporte=0x7f08000f;
+        public static final int tvReporteActivityPorcentaje=0x7f080010;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int activity_reporte=0x7f030001;
-        public static final int listview_gasto=0x7f030002;
-        public static final int listview_reporte=0x7f030003;
+        public static final int activity_gastos=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_reporte=0x7f030002;
+        public static final int listview_gasto=0x7f030003;
+        public static final int listview_reporte=0x7f030004;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
-        public static final int reporte=0x7f070001;
+        public static final int gastos=0x7f070000;
+        public static final int main=0x7f070001;
+        public static final int reporte=0x7f070002;
     }
     public static final class string {
+        public static final int aceptar=0x7f05000e;
         public static final int action_settings=0x7f050002;
+        public static final int administrar=0x7f05000c;
         public static final int app_name=0x7f050000;
+        public static final int cancelar=0x7f05000d;
         public static final int categoria=0x7f050008;
         public static final int hello_world=0x7f050001;
         public static final int importe=0x7f050009;
@@ -71,6 +82,7 @@ public final class R {
         public static final int reportes=0x7f050004;
         public static final int seleccione_anio=0x7f050007;
         public static final int seleccione_mes=0x7f050006;
+        public static final int title_activity_gastos=0x7f05000b;
         public static final int title_activity_reporte=0x7f050005;
     }
     public static final class style {
