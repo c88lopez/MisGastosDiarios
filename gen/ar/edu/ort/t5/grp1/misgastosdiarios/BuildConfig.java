34e28c7622877b4c8b8b77586efffb3a02e34453
Chao <Leandro.Chao@turner.com>
2017-06-06 23:25:20 -0300

    .gitignore is now working

--- gen/ar/edu/ort/t5/grp1/misgastosdiarios/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package ar.edu.ort.t5.grp1.misgastosdiarios;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
