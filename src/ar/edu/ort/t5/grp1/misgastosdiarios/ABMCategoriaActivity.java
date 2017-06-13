bf1a377bc3531dd638b844652a31a15c6cd3e178
33861460 <33861460@A-CRO-04.ALMAGRO.ort.arg>
2017-06-13 19:56:41 -0300

    Se agrega funcionalidad

--- src/ar/edu/ort/t5/grp1/misgastosdiarios/ABMCategoriaActivity.java
+++ src/ar/edu/ort/t5/grp1/misgastosdiarios/ABMCategoriaActivity.java
@@ -1,16 +1,40 @@
 package ar.edu.ort.t5.grp1.misgastosdiarios;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import ar.edu.ort.t5.grp1.data.CategoriaData;
 
 public class ABMCategoriaActivity extends Activity {
 
+	Categoria cate;
+	EditText etDescripcion;
+	Button btnBorrar;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_abmcategoria);
+		
+		etDescripcion = (EditText) findViewById(R.id.etABMCategoriaDescripcion);
+		btnBorrar = (Button) findViewById(R.id.btnABMCategoriaActivityBorrar);
+		//Capturar datos por parametro del listViewCategoria
+		//Si llega por parametro es una modificacion (update)
+		if(cate == null)
+		{//Nuevo
+			btnBorrar.setVisibility(0);
+		}else{ //Modificacion
+			etDescripcion.setText(cate.getDescripcion());
+			btnBorrar.setVisibility(0);
+		}
+		
 	}
 
 	@Override
@@ -30,5 +54,78 @@ public class ABMCategoriaActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	public void btnABMCategoriaActivityAceptar(View view){
+		if(ValidarCampos())
+		{
+			try{
+				CategoriaData cd = new CategoriaData(this);
+				if (cate == null)
+				{//nuevo
+					cd.insert(new Categoria(String.valueOf(etDescripcion.getText())));
+				}else
+				{//modificacion
+					cd.update(cate);
+				}
+				MostrarMensaje("Categoria registrada correctamente");
+				IrACategoriaActivity();
+			}
+			catch (Exception ex)
+			{
+				MostrarMensaje(ex.getMessage(),Toast.LENGTH_LONG);
+			}
+		}
+	}
+	
+	public void btnABMCategoriaActivityCancelar(View view){
+		IrACategoriaActivity();
+	}
+	
+	public void btnABMCategoriaActivityBorrar(View view){
+		try{
+			CategoriaData cd = new CategoriaData(this);
+			cd.delete(cate);
+		}
+		catch(Exception ex){
+			MostrarMensaje(ex.getMessage(),Toast.LENGTH_LONG);
+		}
+		IrACategoriaActivity();
+	}
+	
+	public void IrACategoriaActivity(){
+		Intent intent = new Intent(this,CategoriaActivity.class);
+		startActivity(intent);
+	}
+	
+	private boolean ValidarCampos()
+	{
+		final int LARGO_DESCRIPCION = 50;
+		
+		if(etDescripcion.getText().toString() == "")
+		{
+			MostrarMensaje("Debe Ingresar una descripcion");
+			return false;
+		} else {
+			if(etDescripcion.getText().length() > LARGO_DESCRIPCION)
+			{
+				MostrarMensaje("El importe no puede superar " + String.valueOf(LARGO_DESCRIPCION) + " caracteres");
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	private void MostrarMensaje(String msj)
+	{
+		MostrarMensaje(msj, Toast.LENGTH_SHORT);
+	}
+	
+	private void MostrarMensaje(String msj, int largo)
+	{
+		Toast toast = Toast.makeText(this, msj, largo );
+        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+        toast.show();
 	}
 }
